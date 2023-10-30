0a71fa830402d595469d4afe1804216c1588be16
j-schmied <jannik.schmied@pm.me>
2023-10-30 15:28:16 +0100

    Updated install

--- install.sh
+++ install.sh
@@ -4,11 +4,14 @@ echo -e "\n\n"
 cat ./assets/banner.txt
 echo -e "\n\n"
 
+# Update database
+brew update
+
 # Install CLI Tools
 brew install afflib aircrack-ng apktool arping autopsy bettercap binwalk bulk_extractor cabextract cadaver chkrootkit crunch cutter darkstat dc3dd dcfldd ddrescue dex2jar dns2tcp dnsmap dnstracer ettercap exiv2 exploitdb fcrackzip foremost fping fragroute freerdp gdb hashcat hping httrack hydra ike-scan jad jd-gui john libewf libpst llvm lynis mac-robber masscan md5deep mdbtools metasploit mitmproxy nasm ncrack nikto nmap ophcrack owasp-zap p0f p7zip pdfcrack pev proxychains-ng proxytunnel ptunnel pwnat radare2 reaver recon-ng rkhunter siege sipp sipsak sleuthkit slowhttptest smali sqlmap ssdeep ssldump sslh sslscan sslsplit sslyze stunnel swaks tcpdump tcpflow tcpreplay theharvester truecrack udptunnel util-linux volatility wireshark yara 
 
 # Install GUI Tools
-brew install --cask armitage burp-suite cutter jad maltego metasploit wireshark zenmap 
+brew install armitage burp-suite cutter jad maltego metasploit wireshark zenmap 
 
 # Install Further Tools
 brew install openvpn tor
